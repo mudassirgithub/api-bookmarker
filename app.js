4f82f067dd0c8a85e9e650b42e0bcf55f5fffc0f
mudassirgitub <amamudassir@gmail.com>
2019-08-22 22:56:57 +0530

    modified app.js

--- app.js
+++ app.js
@@ -6,26 +6,40 @@ var cors = require('cors');
 var app = express();
 
 app.use(cors());
+app.use(express.json())
 
 app.get('/', function (req, res) {
     res.send(JSON.stringify({ Hello: 'This is api-bookmarker app' }));
 });
 
-app.route('/url')
-    .post(function (req, res) {
-        const url = {
-            urlgot: req.body.urlgot
-        }
-        unfurled(url.urlgot)
+app.post('/url', function (req, res) {
+    var urlgot = req.body.urlgot
+    if (urlgot === null) {
+        res.json({
+            hasError: true,
+            errorMsg: "got empty filed"
+        })
+    }
+    else {
+        unfurled(urlgot)
             .then(resp => {
                 console.log(resp);
                 res.json(resp)
             })
-            .catch(err => console.log(err))
-    })
-    .get(function (req, res) {
-        res.send(JSON.stringify({ Message: 'post a url to this route to get unfurled info' }));
-    })
+            .catch(err => {
+                console.log(err)
+                res.json({
+                    hasError: true,
+                    errorMsg: "Invalid Url"
+                })
+            })
+    }
+});
+
+app.get('/url', function (req, res) {
+    res.send(JSON.stringify({ message: 'to get unfurled info post url to this route' }));
+});
+
 
 app.listen(port, function () {
     console.log('api-bookmarker app listening on port !');
